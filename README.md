f631f636f262d4e3caa6980153b63ebf7bbc64d6
Cristiano <cristiano.sar@gmail.com>
2021-06-19 15:07:00 -0300

    adjustments

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # sitescrapper
 Simple Beautiful Soup Website scrapper
 Specifically used as a utily to download magazines from the worldradiohistory.com website
+
+# Instructions
+- Make sure you have anaconda installed
+- create and activate the anaconda virtual environment
+```
+- ./create-conta-env.sh
+- conda activate scrapper
+```
+- install the requirements
+```
+pip install -r requirements.txt
+
+```
+- adapt the code according to your needs
+- run the code
+```
+python index.py
+
+```
--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,22 +6,23 @@ import requests
 # BASE_URL = "https://worldradiohistory.com/Archive-Short-Wave-Television/30s/"
 
 BASE_URL = "https://worldradiohistory.com/Archive-Short-Wave-Television/40s/"
+DOWNLOAD_DIRECTORY = "worldradio_40s"
 
 response = requests.get(BASE_URL)
 page_html = response.text
-soup = BeautifulSoup(page_html, 'html.parser')
+soup = BeautifulSoup(page_html, "html.parser")
 file_links = soup.select("td a")
 
-def download(url, directory):
-	file_name = url.rsplit('/', 1)[1]
+def download(url):
+	file_name = url.rsplit("/", 1)[1]
 	r = requests.get(url, allow_redirects=True)
-	open(directory + '/' + file_name, 'wb').write(r.content)
+	if not os.path.exists(DOWNLOAD_DIRECTORY):
+		os.makedirs(DOWNLOAD_DIRECTORY)
+	open(DOWNLOAD_DIRECTORY + "/" + file_name, "wb").write(r.content)
 
 for link in file_links:
-	href = link['href']
+	href = link["href"]
 	if ".pdf" in href:
-		full_url = BASE_URL + link['href']
-		#download(full_url, 'worldradio_30s')
-		download(full_url, 'worldradio_40s')
-
-
+		full_url = BASE_URL + link["href"]
+		print("Downloading: " + full_url)
+		download(full_url)
